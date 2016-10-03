80d410869e99db84c22d9db746ce8e2ffa952aaa
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2016-10-02 23:34:25 -0300

    Add port

--- controllers/espelhos.py
+++ controllers/espelhos.py
@@ -6,7 +6,10 @@ import collections
 import simplejson as json
 
 INTERFACE = 'eth0'
+PORT = '5683'
 NAME = 'espelhos-' + str(randint(100000, 999999))
+
+RegisterMessage = collections.namedtuple('RegisterMessage', ['name', 'port'])
 
 class Espelhos(object):
 
@@ -16,14 +19,15 @@ class Espelhos(object):
         self.server_address = server_address
 
     def register(self):
-        req = self.register_request()
+        message = RegisterMessage(name=NAME, port=PORT)
+        req = self.register_request(message)
         self.logger.info('Register created made: %s', str(req))
 
         with request.urlopen(req) as response:
             pass
 
-    def register_request(self):
-        data = str.encode(json.dumps({'name': NAME}))
+    def register_request(self, message):
+        data = str.encode(json.dumps(message))
         url = self.server_address + '/box'
         req = request.Request(url=url, data=data, method='POST')
         req.add_header('Content-Type', 'application/json')
